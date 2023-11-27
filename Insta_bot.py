644746cc901c398d01ecf1a348b3681a7612cc64
Kx09 <karimahsan909@gmail.com>
2023-11-26 22:19:05 -0800

    Minor Bug fixes

--- Insta_bot.py
+++ Insta_bot.py
@@ -17,7 +17,7 @@ def OpenBrowser():#Opens browser(Firefox) and returns driver
 
     return driver 
 
-def enterPasword(driver):#enters given password
+def enterPasword(driver,username,password):#enters given password
 
     # Find username and password fields and submit button
     username_input = driver.find_element("name", "username")
@@ -27,15 +27,15 @@ def enterPasword(driver):#enters given password
 
     # Enter your login information
 
-    username_input.send_keys("kevenanderson66")
-    password_input.send_keys("instagram@909")
+    username_input.send_keys(username)
+    password_input.send_keys(password)
 
     # Click the login button
     login_button.click()
     time.sleep(3)
 
 
-def removeOuterloop(driver,profiles_keep):
+def removeOuterloop(driver,profiles_keep):#outer loop goes through 
 
     time.sleep(2)
     profile_button = driver.find_elements("class name","x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft")
@@ -74,11 +74,11 @@ def removeOuterloop(driver,profiles_keep):
      
 def remove_following(buts,driver,profiles_keep):
             
-
+            #waits for button click 
             buts.click()
             time.sleep(2)
 
-
+            #finds every button for 
             prof_class = convert_class_name("_ap3a _aaco _aacw _aacx _aad7 _aade")
             followings = driver.find_elements("class name",prof_class)
             time.sleep(3)
@@ -93,19 +93,25 @@ def remove_following(buts,driver,profiles_keep):
 
             counter = 0 
 
+            find_num_followers = convert_class_name("html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs")
+            nums = driver.find_elements("class name",find_num_followers)
+
+
+            num_followers = int(nums[2].text)
+            print(f"The number of following is {num_followers}")
 
             
-            for profile in profile:
-                if profile.text in profiles_keep:
-                        continue
-                        counter = counter + 1
+            for i in range(num_followers):
+                if followings[i].text in profiles_keep:
+                        i = i + 1
                         time.sleep(2)
+                        continue
                 else:
-                    removes[counter].click()
+                    removes[i].click()
                     time.sleep(1)
                     clsname = convert_class_name("_a9-- _ap36 _a9-_")
                     unfollow_button = driver.find_elements("class name","_a9-- _ap36 _a9-_")
-                    del removes[counter]
+                    del removes[i]
                     time.sleep(2)
 
                                         
@@ -125,24 +131,40 @@ def remove_followers(buts,driver,profiles_keep):
             prof_class = convert_class_name("_ap3a _aaco _aacw _aacx _aad7 _aade")
             profiles = driver.find_elements("class name",prof_class)
 
-                
-            counter = 0 
             name_removes = convert_class_name("x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x16n37ib x1uhb9sk x1plvlek xryxfnj x1c4vz4f xs83m0k x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1")
             removes = driver.find_elements("class name",name_removes)
 
-            del removes[0]
-            del removes[1]
+    
+
+            print(removes)
 
             
             counter = 0
+           
+            find_num_followers = convert_class_name("html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs")
+            nums = driver.find_elements("class name",find_num_followers)
 
-            for profile in profiles:
-                if profile.text in profiles_keep:
-                        continue
+
+            num_followers = int(nums[1].text)
+            print(f"The number of following is {num_followers}")
+
+            
+
+            for i in range(num_followers):
+                print(profiles[counter].text)
+                if profiles[counter].text in profiles_keep:
                         counter = counter + 1
+                        continue
                 else:
                     removes[counter].click()
                     del removes[counter]
+                    try:
+                        remove_button = driver.find_element("class name","_a9--._ap36._a9-_")
+                        remove_button.click()
+                        time.sleep(1)
+                    except:
+                         print("Could not find remove button, possible delay")
+                    counter = counter + 1
                 
 
                         
@@ -158,15 +180,15 @@ def convert_class_name(string):
     return class_name_clean
 
 
-
-
 def main():
     driver = OpenBrowser()
-    enterPasword(driver)
+    username = "maplechaipod"
+    password = "aoanazareen123"
+    enterPasword(driver,username,password)
 
 
 
-    profiles_keep = ["pratt_ham","sarth4k25"]
+    profiles_keep = ["krem808s"]
     removeOuterloop(driver,profiles_keep)
 
 
