0ed13f3ce70520b8640e6c77d25f5930ab557278
Kx09 <karimahsan909@gmail.com>
2023-11-26 20:18:06 -0800

    Added functions, open browswer, remove outerloop and remove_followers

--- Insta_bot.py
+++ Insta_bot.py
@@ -8,8 +8,6 @@ from selenium.webdriver.firefox.service import Service
 import time 
 
 
-
-
 def OpenBrowser():#Opens browser(Firefox) and returns driver 
     geckodriver_path = r'C:\Program Files\Gekodriver'  # Update this path
     service = Service(geckodriver_path)
@@ -18,8 +16,6 @@ def OpenBrowser():#Opens browser(Firefox) and returns driver
     time.sleep(2)
 
     return driver 
-
-
 
 def enterPasword(driver):#enters given password
 
@@ -39,22 +35,139 @@ def enterPasword(driver):#enters given password
     time.sleep(3)
 
 
-def findprofile(driver):
-    profile_button = driver.find_element("xpath", "//button[contains(text(), 'Profile')]")
-    profile_button.click()
+def removeOuterloop(driver,profiles_keep):
+
+    time.sleep(2)
+    profile_button = driver.find_elements("class name","x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft")
+
+    for button in profile_button:
+        #print(button.text)
+        if button.text == "Profile":
+            button.click()
+
+    
     time.sleep(2)
 
-    followers_butt = driver.find_element("xpath", "//button[contains(text(), 'Followers')]")
-    followers_butt.click()
-    time.sleep(2)
-
-    removes = driver.find_elements("xpath", "//button[contains(text(), 'Remove')]")
-    print(removes)
 
 
 
+    class_name = convert_class_name("x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd")
+    followers_n_following = driver.find_elements("class name",class_name)
 
-driver = OpenBrowser()
-enterPasword(driver)
-findprofile(driver)
+    print(followers_n_following)
 
+
+    once = True 
+
+    for buts in followers_n_following:
+        if once == True:
+             remove_followers(buts,driver,profiles_keep)
+             once = False
+
+
+        else:
+             remove_following(buts,driver,profiles_keep)
+
+
+                
+        
+     
+def remove_following(buts,driver,profiles_keep):
+            
+
+            buts.click()
+            time.sleep(2)
+
+
+            prof_class = convert_class_name("_ap3a _aaco _aacw _aacx _aad7 _aade")
+            followings = driver.find_elements("class name",prof_class)
+            time.sleep(3)
+
+                
+
+            name_removes = convert_class_name("_ap3a _aaco _aacw _aad6 _aade")
+            removes = driver.find_elements("class name",name_removes)
+
+            time.sleep(2)
+
+
+            counter = 0 
+
+
+            
+            for profile in profile:
+                if profile.text in profiles_keep:
+                        continue
+                        counter = counter + 1
+                        time.sleep(2)
+                else:
+                    removes[counter].click()
+                    time.sleep(1)
+                    clsname = convert_class_name("_a9-- _ap36 _a9-_")
+                    unfollow_button = driver.find_elements("class name","_a9-- _ap36 _a9-_")
+                    del removes[counter]
+                    time.sleep(2)
+
+                                        
+            cross = driver.find_element("class name","_abm0")
+            cross.click()
+                       
+
+
+
+
+def remove_followers(buts,driver,profiles_keep):
+            
+            profile_postition = 0
+            buts.click()
+            time.sleep(2)
+
+            prof_class = convert_class_name("_ap3a _aaco _aacw _aacx _aad7 _aade")
+            profiles = driver.find_elements("class name",prof_class)
+
+                
+            counter = 0 
+            name_removes = convert_class_name("x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x16n37ib x1uhb9sk x1plvlek xryxfnj x1c4vz4f xs83m0k x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1")
+            removes = driver.find_elements("class name",name_removes)
+
+            del removes[0]
+            del removes[1]
+
+            
+            counter = 0
+
+            for profile in profiles:
+                if profile.text in profiles_keep:
+                        continue
+                        counter = counter + 1
+                else:
+                    removes[counter].click()
+                    del removes[counter]
+                
+
+                        
+            cross = driver.find_element("class name","_abm0")
+            cross.click()
+            once = False            
+
+
+def convert_class_name(string):
+    following_string = string
+    class_name_clean= following_string.replace(" ",".")
+
+    return class_name_clean
+
+
+
+
+def main():
+    driver = OpenBrowser()
+    enterPasword(driver)
+
+
+
+    profiles_keep = ["pratt_ham","sarth4k25"]
+    removeOuterloop(driver,profiles_keep)
+
+
+main()
---
+++ README.md
@@ -0,0 +1,21 @@
+
+"""
+
+Runs on python selenium 
+Opens instagram web, goes through each profile, unfollows every single one
+except the specified profiles
+
+pip install selenium 
+
+download geekodriver:
+1)make sure it is the same installation as your browswer
+2)make it 32 or 64
+
+path to geekodriver in enviourment variables
+1)search for enviourment variables 
+2)add their
+3) possibliy restart computer 
+
+
+
+"""
